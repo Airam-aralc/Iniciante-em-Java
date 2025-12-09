96d11d2a873becdc4e4a9d483ee25d0c8964664a
Maria Clara <mari.claara04@gmail.com>
2025-12-09 18:15:11 -0300

    Delete programa/Java_aplicando.orientacao.objetos/modulo1 directory

--- programa/Java_aplicando.orientacao.objetos/modulo1/src/Filme.java
+++
@@ -1,8 +0,0 @@
-public class Filme { // A Classe especifíca o conteúdo de um Objeto
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
-    double avaliacao;
-    int totalDeAvaliacao;
-    int duracaoEmMinutos;
-}
