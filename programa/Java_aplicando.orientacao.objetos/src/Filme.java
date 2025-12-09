eebeee389fbe71306102a4640e9bc9a37d6b8642
Maria Clara <mari.claara04@gmail.com>
2025-12-09 11:36:36 -0300

    Aula sobre orientação a objetos em Java
    Estou aprendendo a usar melhor o Intellij, vou organizar agora cada curso em uma pasta só.

---
+++ programa/Java_aplicando.orientacao.objetos/src/Filme.java
@@ -0,0 +1,8 @@
+public class Filme { // A Classe especifíca o conteúdo de um Objeto
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacao;
+    int duracaoEmMinutos;
+}
