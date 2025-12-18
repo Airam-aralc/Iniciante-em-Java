a43831574b34e16d35158199db3d8b741121fccc
Maria Clara <mari.claara04@gmail.com>
2025-12-18 15:22:16 -0300

    modificação do nome do podcast no módulo 5

--- programa/Java_aplicando.orientacao.objetos/modulo5/src/Principal.java
+++ programa/Java_aplicando.orientacao.objetos/modulo5/src/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 
     public static void main (String[] args){
         Podcast podcast = new Podcast();
-        podcast.setTituloPodcast("Como a tecnologia muda o mundo");
+        podcast.setTituloPodcast("Como a tecnologia muda o mundo?");
         podcast.setDuracao(55000);
         podcast.setTotalDeReproducao(10000);
         podcast.setCurtidas(5000);
