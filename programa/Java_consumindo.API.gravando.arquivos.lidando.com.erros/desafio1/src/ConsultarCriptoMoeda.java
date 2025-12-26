280a95ec58ac5e47ec6bf78391590800233284d2
Maria Clara <mari.claara04@gmail.com>
2025-12-26 14:24:32 -0300

    conclusão de todos os desafios do módulo 1

--- programa/Java_consumindo.API.gravando.arquivos.lidando.com.erros/desafio1/src/ConsultarCriptoMoeda.java
+++ programa/Java_consumindo.API.gravando.arquivos.lidando.com.erros/desafio1/src/ConsultarCriptoMoeda.java
@@ -1,18 +1,18 @@
-import java.io.IOException; //adicionou uma exceção por causa do send
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-public class ConsultarLivros{
+public class ConsultarCriptoMoeda {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite um livro para busca"); //Se for um título composto tem que colocar o + para separar as palavras, não o espaço
+        System.out.println("Digite a criptomoeda para busca:");
         var busca = leitura.nextLine();
 
-        String endereco = "https://www.googleapis.com/books/v1/volumes?q=" + busca + "&apikey=AIzaSyB1pEZIMa_K8UKf9baqKQhq1sSxBiLz1_U"; //minha chave API gerei no google Book: AIzaSyB1pEZIMa_K8UKf9baqKQhq1sSxBiLz1_U
+        String endereco = "https://api.coingecko.com/api/v3/simple/price?ids=" + busca + "&vs_currencies=usd";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
--- programa/Java_consumindo.API.gravando.arquivos.lidando.com.erros/desafio1/src/ConsultarReceita.java
+++ programa/Java_consumindo.API.gravando.arquivos.lidando.com.erros/desafio1/src/ConsultarReceita.java
@@ -1,18 +1,18 @@
-import java.io.IOException; //adicionou uma exceção por causa do send
+import java.io.IOException; 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-public class ConsultarLivros{
+public class ConsultarReceita {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite um livro para busca"); //Se for um título composto tem que colocar o + para separar as palavras, não o espaço
+        System.out.println("Digite o nome da receita para busca:");
         var busca = leitura.nextLine();
 
-        String endereco = "https://www.googleapis.com/books/v1/volumes?q=" + busca + "&apikey=AIzaSyB1pEZIMa_K8UKf9baqKQhq1sSxBiLz1_U"; //minha chave API gerei no google Book: AIzaSyB1pEZIMa_K8UKf9baqKQhq1sSxBiLz1_U
+        String endereco = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + busca; //não precisou da chave nesse site
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
