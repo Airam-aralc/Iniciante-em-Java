755556246f8c27857c056bd67280d9d2abcd09cf
Maria Clara <mari.claara04@gmail.com>
2025-12-27 11:36:54 -0300

    correção do desafio do módulo 2

--- programa/Java_consumindo.API.gravando.arquivos.lidando.com.erros/desafio2/src/Principal.java
+++ programa/Java_consumindo.API.gravando.arquivos.lidando.com.erros/desafio2/src/Principal.java
@@ -1,8 +1,19 @@
-
+import com.google.gson.Gson;
 
 public class Principal {
 
     public static void main (String[] args){
-        String jsonPesssoa = "{\"nome\":\"Rodrigo"}";
+        String jsonPesssoa = """
+                {  
+                    "nome": "Maria",
+                    "idade": 21,
+                    "cidade": "Belo Horizonte"
+                }
+                """; //Text Blocks
+
+        Gson gson = new Gson(); //importou o Gson do google pelo "Project Structure"
+        Pessoa pessoa = gson.fromJson(jsonPesssoa, Pessoa.class);
+
+        System.out.println("Objeto pessoa: " + pessoa);
     }
 }
