39653fdaeb22fca04b81af0a51aa348c5dc2f792
Maria Clara <mari.claara04@gmail.com>
2026-01-05 15:25:39 -0300

    continuação do módulo 1

--- programa/Java_lambdas.streams.SpringFramework/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ programa/Java_lambdas.streams.SpringFramework/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,11 +18,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         var consumoApi = new ConsumoApi(); //var (inferência de tipo). O compilador Java é capaz de determinar o tipo da variável com base no tipo do objeto que está sendo atribuído a ela.
-        var json = consumoApi.obterDados("http://www.omdbapi.com/?t=lost&Season=1&apikey=463d14c9");
+        var json = consumoApi.obterDados("http://www.omdbapi.com/?t=lost&apikey=463d14c9");
         System.out.println(json);
-        //json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-        //System.out.println(json);
 
-
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
     }
 }
--- programa/Java_lambdas.streams.SpringFramework/screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ programa/Java_lambdas.streams.SpringFramework/screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -1,7 +1,9 @@
 package br.com.alura.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias; //como um apelido
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true) //ignora o que não queremos, só nos interessamos por título, total de temporadas e avaliação
 public record DadosSerie(@JsonAlias ("Title") String titulo,
                          @JsonAlias ("totalSeasons") Integer totalTemporadas,
                          @JsonAlias ("imdbRating") String avaliacao) {
