71ca3700b12bd7efb8d2a84f7a22a7b55a74002f
Maria Clara <mari.claara04@gmail.com>
2026-01-05 12:20:34 -0300

    teoria módulo 1

--- programa/Java_lambdas.streams.SpringFramework/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ programa/Java_lambdas.streams.SpringFramework/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +15,12 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Primeiro projeto Spring sem o Web");
+        var consumoApi = new ConsumoApi(); //var (inferência de tipo). O compilador Java é capaz de determinar o tipo da variável com base no tipo do objeto que está sendo atribuído a ela.
+        var json = consumoApi.obterDados("http://www.omdbapi.com/?t=lost&Season=1&apikey=463d14c9");
+        System.out.println(json);
+        //json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+        //System.out.println(json);
+
+
     }
 }
