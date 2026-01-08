1ca04d0a3824838d0f4057a8df2874f83028641d
Maria Clara <mari.claara04@gmail.com>
2026-01-08 00:45:07 -0300

    finalização da parte teórica do módulo 2

--- programa/Java_lambdas.streams.SpringFramework/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ programa/Java_lambdas.streams.SpringFramework/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,49 +3,88 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
+    //As variáveis de instância ficam fora dos métodos
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
     private ConverteDados conversor = new ConverteDados();
 
-    //Usamos o "final" e letras maiúsculas para constantes, naõ iremos modificar depois
     private final String ENDERECO = "http://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=463d14c9";
 
-    public void exibeMenu(){
+    //O metodo exibeMenu() é um metodo da classe, não do main
+    public void exibeMenu() {
         System.out.println("Digite o nome da série: ");
         var nomeSerie = leitura.nextLine();
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
 
-
         List<DadosTemporada> temporadas = new ArrayList<>();
 
-        for (int i = 1; i<= dados.totalTemporadas(); i++){
-            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + "&" + API_KEY);
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
             DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
             temporadas.add(dadosTemporada);
         }
-        temporadas.forEach(System.out::println); //Cada elemento da lista será impresso
 
-//        for (int i = 0; i < dados.totalTemporadas(); i++){
-//            List<DadosEpisodio> episodiosTemporadas = temporadas.get(i).episodio();
-//            //Irá pegar somente os títulos da série
-//            for (int j = 0; j < episodiosTemporadas.size(); j++){
-//                System.out.println(episodiosTemporadas.get(j).titulo());
-//            }
-//        }
-
-        // ( -> ) São lambdas, usados para trabalhar com coleções
+        temporadas.forEach(System.out::println);
         temporadas.forEach(t -> t.episodio().forEach(e -> System.out.println(e.titulo())));
+
+        //flatMap irá criar uma lista dentro de outra lista
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodio().stream())
+                .collect(Collectors.toList());
+
+        //Pegar os cinco melhores episodios
+        System.out.println("\nTOP 5 episódios:");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodio().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano você quer ver os episódios?");
+        var ano = leitura.nextInt(); // Usamos nextInt para capturar o número diretamente
+        leitura.nextLine(); // Limpamos o buffer após ler o número
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " | Episódio: " + e.getTitulo() +
+                                " | Data de lançamento: " + e.getDataLancamento().format(formatador)
+                ));
+
+    }
+
+    //O main apenas inicia a execução
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.exibeMenu();
     }
 }
