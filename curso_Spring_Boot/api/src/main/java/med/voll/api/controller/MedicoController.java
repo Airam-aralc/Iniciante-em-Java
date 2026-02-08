d654d3b68fdc10a202ee4a7d1583157bf8deb2a6
Maria Clara <mari.claara04@gmail.com>
2026-02-07 22:56:22 -0300

    conexão com o SQL

--- curso_Spring_Boot/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ curso_Spring_Boot/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,22 @@
 package med.voll.api.controller;
 
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedicos;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController //faz a comunicação Spring
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired //Injeção de dependências
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedicos dados){ //para saber que esse json está vindo do corpo da requisição
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 }
