d3decb19a90b1e0cd38b42ef7552d95470d8f968
Maria Clara <mari.claara04@gmail.com>
2026-02-08 19:46:28 -0300

    continuação da conexão com o SQL

--- curso_Spring_Boot/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ curso_Spring_Boot/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,13 +1,15 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedicos;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController //faz a comunicação Spring
 @RequestMapping("medicos")
@@ -22,4 +24,8 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
 
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    }
 }
