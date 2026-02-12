78c961c3da80a69b63543b5c1ff629ae2cc73357
Maria Clara <mari.claara04@gmail.com>
2026-02-12 12:06:22 -0300

    ajuste na geração do token

---
+++ curso_Spring_Boot/api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,38 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}") //do application.properties
+    private String secret;
+
+    public String gerarToken(Usuario usuario){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao()) //é recomendado colocar uma data de Expiração para o Token para que o acesso do usuário não caia em mãos erradas
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
+    }
+
+    //o token vai expirar depois de 2 horas que o usuário utilizar
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
